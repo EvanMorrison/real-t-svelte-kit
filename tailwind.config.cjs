b217b5bb02055023ab3e28658d67f9a4da64f50e
Evan Morrison <evanmorrison@filevine.com>
2021-12-21 17:24:48 -0700

    create home page HTML & styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,svelte}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Roboto', 'Helvetica-Neue', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
