f0b96b6cc5ebcae7101e7c7a38e8e2e8464dfe00
Evan Morrison <evanmorrison@filevine.com>
2021-12-30 23:08:13 -0700

    add contact page and endpoints

--- src/lib/components/addressBlock.svelte
+++ src/lib/components/addressBlock.svelte
@@ -2,8 +2,7 @@
 	import InputField from './inputField.svelte';
 
 	interface Address {
-		street1?: string;
-		street2?: string;
+		street?: string;
 		city?: string;
 		state?: string;
 		zipCode?: string;
@@ -15,8 +14,7 @@
 
 {#if isEditing}
 	<div>
-		<InputField id="street1" label="Street 1" bind:value={address.street1} />
-		<InputField id="street2" label="Street 2" bind:value={address.street2} />
+		<InputField id="street" label="Street" bind:value={address.street} />
 		<div class="flex gap-4 max-w-full">
 			<div class="flex-1">
 				<InputField id="city" label="City" bind:value={address.city} />
@@ -30,10 +28,7 @@
 		</div>
 	</div>
 {:else}
-	<div>{address.street1}</div>
-	{#if address.street2}
-		<div>{address.street2}</div>
-	{/if}
+	<div>{address.street}</div>
 	<div class="flex gap-4 max-w-full">
 		<div class="flex-1">{address.city}</div>
 		<div class="basis-[80px] flex-none">{address.state}</div>
--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -4,29 +4,13 @@
 	const user: CurrentUser = getContext('currentUser');
 </script>
 
-<div
-	class="bg-indigo-500 p-2 text-white h-[50vh] relative flex flex-col justify-between  items-center"
->
-	<div class="self-start mt-[5rem] ml-[5rem]">
-		<h1 class="text-9xl font-extralight -ml-2.5">REAL-T</h1>
-		<h3>Real Estate Transaction Manager</h3>
-	</div>
+<div class="bg-indigo-500 p-2 h-52 text-white relative flex flex-col justify-end  items-center">
 	<div class="text-center">
 		<p class="text-3xl">Welcome!</p>
 		<p>Let's Get Started.</p>
 	</div>
 </div>
-<div class="flex justify-end p-2">
-	<a class="text-inherit visited:text-inherit" href="/app/account"
-		><span class="material-icons-round align-middle text-xl">manage_accounts</span>
-		{(user && user.firstName) || user.email || 'Account'}
-	</a>
-	&nbsp; | &nbsp;
-	<a class="text-inherit visited:text-inherit" href="/logout.json" rel="external"
-		><span class="material-icons-round align-middle text-xl">logout</span> log out</a
-	>
-</div>
-<div>
+<div class="pt-8">
 	<div>
 		<div class="flex justify-around py-8">
 			<div class="grid-item">
@@ -35,16 +19,22 @@
 					<span class="material-icons-round home-icon">view_list</span>
 				</a>
 			</div>
-			<!-- <div class="grid-item">
-				<a class="main-nav-links" href="app/dashboard">
-					<h1>Case Dashboard</h1>
-					<span class="material-icons-round home-icon">chrome_reader_mode</span>
-				</a>
-			</div> -->
 			<div class="grid-item">
 				<a class="main-nav-links" href="app/project-create">
 					<h1>New Project</h1>
 					<span class="material-icons-round home-icon">create_new_folder</span>
+				</a>
+			</div>
+			<div class="grid-item">
+				<a class="main-nav-links" href="app/contacts">
+					<h1>Contacts</h1>
+					<span class="material-icons-round home-icon">people</span>
+				</a>
+			</div>
+			<div class="grid-item">
+				<a class="main-nav-links" href="app/property">
+					<h1>Property</h1>
+					<span class="material-icons-round home-icon">terrain</span>
 				</a>
 			</div>
 		</div>
